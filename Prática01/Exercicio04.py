50a29c32a1faa364b43e957da9a909a57ce8c933
Lucyanna <coelholucyanna@gmail.com>
2025-05-16 19:24:04 -0400

    exercicios da pratica01

---
+++ Prática01/Exercicio04.py
@@ -0,0 +1,5 @@
+nomedoproduto = "cadeira infantil"
+precounitario = 12.40
+quantidade = 3
+total = precounitario * quantidade
+print("O produto", nomedoproduto, "custa R$", precounitario, "e você comprou", quantidade, "unidades. O total é R$", total)
